05729d9651a419581e3b987afd27d2153ac7d6d1
kosior <krzysiek.kosiorek@gmail.com>
2018-01-29 17:16:32 +0100

    add cart item count to navbar

--- FE/src/app/bs-navbar/bs-navbar.component.ts
+++ FE/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,18 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth/auth.service';
 import {Observable} from 'rxjs/Observable';
 import {User} from '../models/user.model';
+import {ShoppingCartService} from '../services/shopping-cart/shopping-cart.service';
 
 @Component({
   selector: 'app-bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   user$: Observable<User>;
+  shoppingCartItemCount: number;
 
-  constructor(private auth: AuthService) {
-    this.user$ = auth.user$;
+  constructor(private auth: AuthService, private cartService: ShoppingCartService) {}
+
+   ngOnInit() {
+    this.user$ = this.auth.user$;
+    this.cartService.items$
+      .subscribe(items => {
+        this.shoppingCartItemCount = 0;
+        for (const item of Object.keys(items)) {
+          this.shoppingCartItemCount += items[item].quantity;
+        }
+      });
   }
 
   logout() {
