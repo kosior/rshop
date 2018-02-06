f2f60dc2aa164c41d9a9bafed9517037f6e2c9bc
kosior <krzysiek.kosiorek@gmail.com>
2018-02-06 12:12:47 +0100

    build check out form; add logic for placing order

--- FE/src/app/check-out/check-out.component.ts
+++ FE/src/app/check-out/check-out.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ShoppingCartService} from '../services/shopping-cart/shopping-cart.service';
+import {Cart} from '../models/cart.model';
+import {Subscription} from 'rxjs/Subscription';
+import {OrderService} from '../services/order/order.service';
+import {Order} from '../models/order.model';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
+  address = {};
+  cart: Cart;
+  cartSubscription: Subscription;
 
-  constructor() { }
+  constructor(private cartService: ShoppingCartService, private orderService: OrderService) {}
 
   ngOnInit() {
+    this.cartSubscription = this.cartService.cart$.subscribe(cart => this.cart = cart);
   }
 
+  ngOnDestroy() {
+    this.cartSubscription.unsubscribe();
+  }
+
+  placeOrder() {
+    const order = new Order(this.address, this.cart);
+    this.orderService.saveOrder(order);
+  }
 }
