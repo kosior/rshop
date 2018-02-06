ab6e241acda881e33db0146552a3112c3076f900
kosior <krzysiek.kosiorek@gmail.com>
2018-02-06 12:15:26 +0100

    cleanup: delete OnDestroy import

--- FE/src/app/products/products.component.ts
+++ FE/src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../services/product/product.service';
 import {ActivatedRoute} from '@angular/router';
 import {Product} from '../models/product.model';
