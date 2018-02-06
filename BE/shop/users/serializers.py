96cf66c47dc91733468fcfd334fcc9c2ee720fae
kosior <krzysiek.kosiorek@gmail.com>
2018-02-06 11:58:06 +0100

    add Address serializer

--- BE/shop/users/serializers.py
+++ BE/shop/users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import User
+from .models import User, Address
 
 
 class UserSerializer(ModelSerializer):
@@ -13,3 +13,9 @@ class UserSerializer(ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class AddressSerializer(ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ('name', 'address_line_1', 'address_line_2', 'city')
