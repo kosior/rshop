423e0c4de4a942d24de360457720f84b22038a74
kosior <krzysiek.kosiorek@gmail.com>
2018-02-14 20:18:23 +0100

    optimize query

--- BE/shop/orders/views.py
+++ BE/shop/orders/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Prefetch
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Order
+from .models import Order, OrderItem
 from .serializers import OrderSerializer
 
 
@@ -8,10 +9,12 @@ class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
 
     def get_queryset(self):
+        prefetch = Prefetch('items', queryset=OrderItem.objects.select_related('product'))
+        qs = Order.objects.select_related('user', 'address').prefetch_related(prefetch)
         user = self.request.user
         if user.is_staff:
-            return Order.objects.all()
-        return Order.objects.filter(user=user)
+            return qs.all()
+        return qs.filter(user=user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
